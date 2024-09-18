c59489f0ebfb446ffeee7be9e6ebee9f4cc05dcb
Tarika <22f3002758@ds.study.iitm.ac.in>
2024-09-18 22:27:11 +0530

    2nd commit

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ class ServiceProvider(db.Model):
     sp_address=db.Column(db.String)
     sp_email=db.Column(db.String,unique=True)
     sp_password=db.Column(db.String)
-    sp_service=db.Column(db.String)
     sp_exp=db.Column(db.Integer)
     sp_phone=db.Column(db.String)
     sp_pincode=db.Column(db.String)
+    sp_service=db.Column(db.String)
+    mypackages=db.relationship("Package", backref="servprovider")
     # sp_camp_child=db.relationship('Campaign')
     # camp_request_sp=db.relationship('Request')  
 
@@ -54,8 +55,9 @@ class Package(db.Model):
     p_id=db.Column(db.Integer, primary_key=True, autoincrement= True)
     p_name=db.Column(db.String,unique=True)
     p_price=db.Column(db.Integer)
+    p_description=db.Column(db.String)
     s_id=db.Column(db.Integer, db.ForeignKey("services.s_id"),nullable=False)
-
+    sp_id=db.Column(db.Integer, db.ForeignKey("serviceprovider.sp_id"),nullable=False)
 
 class Booking(db.Model) :
     __tablename__="booking"
@@ -65,6 +67,10 @@ class Booking(db.Model) :
     sp_id=db.Column(db.Integer, db.ForeignKey("serviceprovider.sp_id"),nullable=False)
     c_id=db.Column(db.Integer, db.ForeignKey("customer.c_id"),nullable=False)
     p_id=db.Column(db.Integer, db.ForeignKey("package.p_id"),nullable=False)
+    b_date=db.Column(db.Date)
+    b_address=db.Column(db.String)
+    b_pincode=db.Column(db.String)
+    b_message=db.Column(db.String)
 
 @app.route("/", methods=["GET","POST"])
 def home():
@@ -89,8 +95,8 @@ def register():
         else:
             cust=Customer(c_name=cname,c_address=caddress, c_pincode=cpincode,c_email=cemail,c_pwd=cpwd,c_phone=cphone)
             db.session.add(cust)
-            db.session.commit    
-            return render_template("login.html")
+            db.session.commit()   
+            return redirect("login.html")
         
 
     elif request.method=="GET" and request.args["utype"]=="serviceprovider":
@@ -103,14 +109,53 @@ def register():
         sppwd=request.form.get("s_pwd")  
         spexp=request.form.get("s_exp") 
         spphone=request.form.get("s_phone")
-        s=db.session.query(ServiceProvider).filter_by(sp_email=spemail).first()
-        if s:
+        sp=db.session.query(ServiceProvider).filter_by(sp_email=spemail).first()
+        if sp:
             return redirect("/exist")
         else:
             sp=ServiceProvider(sp_name=spname,sp_address=spaddress, sp_pincode=sppincode,sp_email=spemail,sp_pwd=sppwd,sp_phone=spphone,sp_exp=spexp)
             db.session.add(sp)
-            db.session.commit    
-            return render_template("login.html")
+            db.session.commit()  
+            return redirect("login.html")
+        
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        email=request.form.get("Email")
+        pwd=request.form.get("Password")
+        c=db.session.query(Customer).filter_by(c_email=email).first()
+        sp=db.session.query(ServiceProvider).filter_by(sp_email=email).first()
+        if c and c.c_password==pwd:
+            return redirect("/customer/Dashboard")
+        elif sp and sp.sp_password==pwd:
+            return redirect("/serviceprovider/Dashboard")
+        else:
+            return render_template("notexist.html")
+
+@app.route("/customer/Dashboard",methods=["GET","POST"])
+def Dashboard():
+    if request.method=="GET":
+        s=db.session.query(Services).all()
+        serv=[]
+        for ser in s:
+            serv.append(ser.s_name)
+        return render_template("/Customer/CustomerDashboard.html",servname=serv)
+
+@app.route("/customer/search",methods=["GET","POST"])
+def search():
+        
+    if request.method=="GET" and "sname" in request.args:
+        s=request.args.get("sname")
+        ser=db.session.query(Services).filter_by(s_name=s).first()
+        pack=ser.packages
+        return render_template("/Customer/custsearch.html",package=pack)
+    
+    elif request.method=="GET":
+        return render_template("/Customer/custsearch.html")
+        
+
 
 if __name__=="__main__":
     app.run(debug=True)
